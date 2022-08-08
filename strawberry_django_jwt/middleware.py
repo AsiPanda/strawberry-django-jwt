925b0cd350fd5139a5edef5be865c57a1ab09384
Nir <88795475+nrbnlulu@users.noreply.github.com>
2022-08-08 11:24:00 +0300

    channels compat - compat JWT headers

--- strawberry_django_jwt/middleware.py
+++ strawberry_django_jwt/middleware.py
@@ -114,6 +114,8 @@ class JSONWebTokenMiddleware(BaseJSONWebTokenMiddleware):
 def channels_compat(context: StrawberryChannelsContext) -> None:
     request = context.request
     request.META = request.headers
+    if auth_header := request.headers.get("authorization"):
+        request.META["HTTP_AUTHORIZATION"] = auth_header
     request.COOKIES = request.scope["session"]
 
 
